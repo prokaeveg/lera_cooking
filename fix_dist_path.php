c79c87488663816c43f2563eeecb7339ddda8916
Egor <prokaev96186903@gmail.com>
2025-06-06 16:24:51 +0300

    удалил упоминания о деплое на рендер, лайвайр компонент заметок

---
+++ fix_dist_path.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types = 1);
+
+require './vendor/autoload.php';
+
+$root = __DIR__ . '/dist';
+$categoryDir = $root . '/category';
+$receiptDir = $root . '/receipt';
+
+$indexFile = $root . '/index.html';
+$content = file_get_contents($indexFile);
+$content = replace_defaults($content, 0);
+$content = preg_replace('#/category/([a-zA-Z-]*)#', 'category/$1/index.html', $content);
+file_put_contents($indexFile, $content);
+
+$categories = glob($categoryDir . '/*');
+foreach ($categories as $category) {
+    $fileName = $category . '/index.html';
+    $content = file_get_contents($category . '/index.html');
+
+    $content = replace_defaults($content);
+    $content = preg_replace('#/receipt/([a-zA-Z-]*)#', '../../receipt/$1/index.html', $content);
+
+    file_put_contents($fileName, $content);
+}
+
+$receipts = glob($receiptDir . '/*');
+foreach ($receipts as $receipt) {
+    $fileName = $receipt . '/index.html';
+    $receiptCode = end(explode('/', $receipt));
+
+    $content = file_get_contents($receipt . '/index.html');
+
+    $content = replace_defaults($content);
+    $content = preg_replace('#/receipts#', "/receipts/$receiptCode.jpg", $content);
+
+    file_put_contents($fileName, $content);
+}
+
+function replace_defaults(string $content, int $nestCount = 2): string
+{
+    $nestString = str_repeat('../', $nestCount);
+
+    $content = preg_replace('#https*://localhost#', '', $content);
+    $content = preg_replace('#https://lera-cooking-latest.onrender.com#', '', $content);
+    $content = preg_replace('#/images#', $nestString . "images", $content);
+    $content = preg_replace('#/fonts#', $nestString . 'fonts', $content);
+    $content = preg_replace('#/favicon.ico#', $nestString . 'favicon.ico', $content);
+
+    return $content;
+}
