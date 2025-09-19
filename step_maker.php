630ee41f900a4e53df15dcd1d07ad27ef046d897
Egor <prokaev96186903@gmail.com>
2025-09-19 21:25:46 +0300

    Ещё рецептики! Жена спасибо за булгур

--- step_maker.php
+++ step_maker.php
@@ -8,9 +8,11 @@ $steps = array_map(
     static function ($step) {
         $step = preg_replace('#\n#', ' ', $step);
         $step = str_replace('\n', ' ', $step);
+        $step = trim($step);
+        $step = trim($step, '.');
         return trim($step);
     },
-    $steps
+    array_filter($steps)
 );
 
 echo json_encode($steps, JSON_UNESCAPED_UNICODE);
